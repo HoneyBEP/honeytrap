20eb5550c2a6eeae5ff7adf9fe63a3856ed5cbd8
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-07 15:11:16 +0200

    Update template and remove unused files

--- lua-scripts/lua/template.lua
+++ lua-scripts/lua/template.lua
@@ -1,39 +1,39 @@
-----------------------------------------
+--- Define if the Lua script can handle the request
 -- canHandle function returns the approval or disapproval to handle the connection. This is based on the information
 -- of the current connection and new request. The service is already determined by the Honeytrap implementation in Go.
-----------------------------------------
-function canHandle()
+-- @param request the request of the connection
+function canHandle(request)
 
 end
 
-----------------------------------------
+--- Define the pre handle for the request
 -- preHandle function is called by the desired service and pre handles the request by the connection. This function can
 -- be used to alter, log or visualize the incoming request in the Honeytrap. From the Honeytrap implementation in Go
 -- there are protocol or service specific functions declared. Take a look in the Honeytrap documentation to check the
 -- availability for standard services. When implementing your own service you can freely implement the preHandle
 -- function in your service.
-----------------------------------------
-function preHandle()
-
+-- @param message command of the request
+function preHandle(message)
+    return message
 end
 
-----------------------------------------
+--- Define the handle of the request
 -- handle function is called by the desired service and handles the request by the connection. From the Honeytrap
 -- implementation in Go there are protocol or service specific functions declared. Take a look in the Honeytrap
 -- documentation to check the availability for standard services. When implementing your own service you can freely
 -- implement the handle function in your service.
-----------------------------------------
+-- @param message command of the request
 function handle(message)
-    return "Hello Http Lua! Your message:"..message..", was received from ".. getRemoteAddr() .." on ".. getDatetime() .."!"
+    return "Hello Lua! Your message:"..message..", was received from ".. getRemoteAddr() .." on ".. getDatetime() .."!"
 end
 
-----------------------------------------
+--- Define the after handle of the request
 -- afterHandle function is called by the desired service and after handles the request by the connection. This function
 -- can be used to alter, log or visualize the outgoing response in the Honeytrap. From the Honeytrap implementation in
 -- Go there are protocol or service specific functions declared. Take a look in the Honeytrap documentation to check the
 -- availability for standard services. When implementing your own service you can freely implement the afterHandle
 -- function in your service.
-----------------------------------------
-function afterHandle()
-
+-- @param message response of the request
+function afterHandle(message)
+    return message
 end
