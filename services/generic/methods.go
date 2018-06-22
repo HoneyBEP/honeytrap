3f0c9bb65c0c4e9ca8aa69260ad6ffe0f51d0032
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-06-22 17:22:44 +0200

    Fix rewrite big methods (#44)
    * Fix non lua extension reading of files
    
    * fix rewrite big methods

--- services/generic/methods.go
+++ services/generic/methods.go
@@ -14,7 +14,69 @@ import (
 
 // setMethods sets the methods required for the generic scripts in the Handle method of the generic service
 func (s *genericService) setMethods(connW scripter.ConnectionWrapper) error {
-	if setErr := connW.SetStringFunction("getRequest", func() string {
+	if setErr := connW.SetStringFunction("getRequest", getRequest(connW)); setErr != nil {
+		 return setErr
+	}
+
+	if setErr := connW.SetVoidFunction("restWrite", restWrite(connW)); setErr != nil {
+		return setErr
+	}
+
+	return nil
+}
+
+//restWrite returns a function that can write a REST response on a connection
+func restWrite(connW scripter.ConnectionWrapper) func() {
+	return func() {
+		params, _ := connW.GetParameters([]string{"status", "response", "headers"})
+
+		status, _ := strconv.Atoi(params["status"])
+		buf := connW.GetScrConn().GetConnectionBuffer()
+		br := bufio.NewReader(buf)
+
+		req, err := http.ReadRequest(br)
+		if err == io.EOF {
+			return
+		} else if err != nil {
+			log.Errorf("Error while reading buffered request connection, %s", err)
+			return
+		}
+
+		defer req.Body.Close()
+
+		header := http.Header{}
+
+		header.Set("date", (time.Now()).String())
+		header.Set("connection", "Keep-Alive")
+		header.Set("content-type", "application/json")
+
+		var headers map[string]string
+		json.Unmarshal([]byte(params["data"]), &headers)
+		for name, value := range headers {
+			header.Set(name, value)
+		}
+
+		resp := http.Response{
+			StatusCode:    status,
+			Status:        http.StatusText(status),
+			Proto:         req.Proto,
+			ProtoMajor:    req.ProtoMajor,
+			ProtoMinor:    req.ProtoMinor,
+			Request:       req,
+			Header:        header,
+			Body:          ioutil.NopCloser(bytes.NewBufferString(params["response"])),
+			ContentLength: int64(len(params["response"])),
+		}
+
+		if err := resp.Write(connW.GetScrConn().GetConn()); err != nil {
+			log.Errorf("Writing of scripter - REST message was not successful, %s", err)
+		}
+	}
+}
+
+//getRequest returns a function that can read in a HTTP request from a connection in JSON
+func getRequest(connW scripter.ConnectionWrapper) func() string {
+	return func() string {
 		params, _ := connW.GetParameters([]string{"withBody"})
 
 		buf := connW.GetScrConn().GetConnectionBuffer()
@@ -59,57 +121,5 @@ func (s *genericService) setMethods(connW scripter.ConnectionWrapper) error {
 		}
 
 		return string(result)
-	}); setErr != nil {
-		 return setErr
 	}
-
-	if setErr := connW.SetVoidFunction("restWrite", func() {
-		params, _ := connW.GetParameters([]string{"status", "response", "headers"})
-
-		status, _ := strconv.Atoi(params["status"])
-		buf := connW.GetScrConn().GetConnectionBuffer()
-		br := bufio.NewReader(buf)
-
-		req, err := http.ReadRequest(br)
-		if err == io.EOF {
-			return
-		} else if err != nil {
-			log.Errorf("Error while reading buffered request connection, %s", err)
-			return
-		}
-
-		defer req.Body.Close()
-
-		header := http.Header{}
-
-		header.Set("date", (time.Now()).String())
-		header.Set("connection", "Keep-Alive")
-		header.Set("content-type", "application/json")
-
-		var headers map[string]string
-		json.Unmarshal([]byte(params["data"]), &headers)
-		for name, value := range headers {
-			header.Set(name, value)
-		}
-
-		resp := http.Response{
-			StatusCode:    status,
-			Status:        http.StatusText(status),
-			Proto:         req.Proto,
-			ProtoMajor:    req.ProtoMajor,
-			ProtoMinor:    req.ProtoMinor,
-			Request:       req,
-			Header:        header,
-			Body:          ioutil.NopCloser(bytes.NewBufferString(params["response"])),
-			ContentLength: int64(len(params["response"])),
-		}
-
-		if err := resp.Write(connW.GetScrConn().GetConn()); err != nil {
-			log.Errorf("Writing of scripter - REST message was not successful, %s", err)
-		}
-	}); setErr != nil {
-		return setErr
-	}
-
-	return nil
 }
